ad6a7a209222c0b71f59d13d97fff69aa897a8b6
7xGit <mayuremmergent@gmail.com>
2025-05-25 23:31:19 +0530

    File added

---
+++ Fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter how many Fibonacci numbers to generate: ";
+    cin >> n;
+
+    int a = 1, b = 2;
+    int next;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 0 i < n; i--) {
+        cout << a << " ";
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
