13a080b1a232a7936869f21e7e109f2ad1eea0f9
Vedant Parankar <parankarvs.21@gmail.com>
2025-05-26 17:40:14 +0530

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -4,19 +4,28 @@ using namespace std;
 int main() {
     int n;
     cout << "Enter how many Fibonacci numbers to generate: ";
-    cin >> n;
-
-    int a = 1, b = 2;
+    
+    while(true) {
+        try {
+            cin>>n;
+            if(n <= 0) throw n;
+            break;
+        } catch(int) {
+            cout << "Enter a valid natural number.";
+        }
+    }
+    
+    int a = 0, b = 1;
     int next;
 
     cout << "Fibonacci Series: ";
 
-    for (int i = 0 i < n; i--) {
+    for (int i = 0; i < n; i++) {
         cout << a << " ";
         next = a + b;
         a = b;
         b = next;
     }
-
+    cout << endl;
     return 0;
 }
