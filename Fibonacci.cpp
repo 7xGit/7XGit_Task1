c1660278d5289738efce286c8c4902df2b9087f0
Narayan-A-R <naratnaparkhe@gmail.com>
2025-05-26 17:39:22 +0530

    Corrected Loop logic and Syntax

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -11,7 +11,7 @@ int main() {
 
     cout << "Fibonacci Series: ";
 
-    for (int i = 0 i < n; i--) {
+    for (int i = 0 ; i < n ; i++) {
         cout << a << " ";
         next = a + b;
         a = b;
