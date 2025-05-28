7a0b2a023348fce4088d13c9f2f5b54ef3aeb136
kartikware <karsonmars42@gmail.com>
2025-05-28 19:17:51 +0530

    Completed Task 1

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -6,12 +6,17 @@ int main() {
     cout << "Enter how many Fibonacci numbers to generate: ";
     cin >> n;
 
-    int a = 1, b = 2;
+    if (n <= 0) {
+        cout << "Enter a positive integer.\n";
+        return 1;
+    }
+
+    int a = 0, b = 1;
     int next;
 
     cout << "Fibonacci Series: ";
 
-    for (int i = 0 i < n; i--) {
+    for (int i = 0; i < n; i++) {
         cout << a << " ";
         next = a + b;
         a = b;
