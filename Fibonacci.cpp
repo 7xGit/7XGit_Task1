d37e4a8552779bfeef9678951047b2e4c00b7f85
Snehal Madhure <snehalmadhure@gmail.com>
2025-05-28 15:18:04 +0530

    Fixed For Loop - Initail Values Changed
    Incrementation and Condition in the for loop were fixed. The initial values were reinitialised to match the outcomes in the documentation.

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -6,12 +6,12 @@ int main() {
     cout << "Enter how many Fibonacci numbers to generate: ";
     cin >> n;
 
-    int a = 1, b = 2;
+    int a = 0, b = 1;
     int next;
 
     cout << "Fibonacci Series: ";
 
-    for (int i = 0 i < n; i--) {
+    for (int i = 0; i < n; i++) {
         cout << a << " ";
         next = a + b;
         a = b;
